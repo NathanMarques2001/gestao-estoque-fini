95b8b2d99b96526c2d02e3320af581ad60a7a335
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2025-04-28 08:57:05 -0300

    feat: primeiro commit

---
+++ back/src/test/java/com/gestao/estoque/fini/FiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.estoque.fini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
